e004b88fb8ddf832324dd86482e4da2714e60b5f
Pedro Sousa <psousa@equalexperts.com>
2019-09-10 17:03:23 +0100

    Design Show Tables layout

--- src/screens/ShowIrnTables.tsx
+++ src/screens/ShowIrnTables.tsx
@@ -1,13 +1,13 @@
+import { Card, Text, View } from "native-base"
+import { sort } from "ramda"
+import React from "react"
 import { SectionList, SectionListData, SectionListRenderItem, StyleSheet } from "react-native"
 import { AppScreen, AppScreenProps } from "../common/AppScreen"
-
-import { Text, View } from "native-base"
-import sort from "ramda/es/sort"
-import React from "react"
 import { mergeIrnTables, sortTimes } from "../irnTables/main"
-import { IrnTableSchedule } from "../irnTables/models"
+import { IrnTableLocation, IrnTableSchedule, Time } from "../irnTables/models"
 import { useGlobalState } from "../state/main"
 import { getCounty, getDistrict, getIrnTables } from "../state/selectors"
+import { formatDate, formatTime, properCase } from "../utils/formaters"
 
 export const ShowIrnTablesScreen: React.FunctionComponent<AppScreenProps> = props => (
   <AppScreen {...props} content={() => <ShowIrnTablesContent {...props} />} title="Agendar CC" showAds={false} />
@@ -17,29 +17,37 @@ const ShowIrnTablesContent: React.FunctionComponent<AppScreenProps> = () => {
   const [globalState] = useGlobalState()
 
   const renderIrnTableSection: SectionListRenderItem<IrnTableSchedule> = ({ item }) => (
-    <View>
-      <Text>{item.date.toString()}</Text>
-      {sort(sortTimes, item.times).map(time => (
-        <Text key={time}>{time}</Text>
-      ))}
+    <View style={styles.scheduleContainer}>
+      <View style={styles.dateContainer}>
+        <Text>{formatDate(item.date)}</Text>
+      </View>
+      <TimesList times={item.times} />
     </View>
   )
-  const renderSectionHeader = (info: { section: SectionListData<IrnTableSchedule> }) => (
-    <View>
-      <Text>HELLO 12345</Text>
-      <Text>{getDistrict(globalState)(info.section.header.county.districtId)!.name}</Text>
-      <Text>{getCounty(globalState)(info.section.header.county.countyId)!.name}</Text>
-    </View>
-  )
+
+  const renderSectionHeader = (info: { section: SectionListData<IrnTableSchedule> }) => {
+    const tableLocation: IrnTableLocation = info.section.tableLocation
+    const district = getDistrict(globalState)(tableLocation.county.districtId)!
+    const county = getCounty(globalState)(tableLocation.county.countyId)!
+
+    return (
+      <Card>
+        <View style={styles.districtContainer}>
+          <Text style={styles.districtText}>{`${district.name} - ${properCase(county.name)}`}</Text>
+          <Text style={styles.locationText}>{tableLocation.locationName}</Text>
+        </View>
+      </Card>
+    )
+  }
 
   const keyExtractor = (_: IrnTableSchedule, index: number) => index.toString()
 
   const { countyId, districtId } = globalState
   const irnTables = getIrnTables(globalState)({ countyId, districtId })
 
-  const mergedTables = mergeIrnTables(irnTables.slice(20, 25))
+  const mergedTables = mergeIrnTables(irnTables)
 
-  const sections = mergedTables.map(table => ({ header: table, data: table.schedules }))
+  const sections = mergedTables.map(tableLocation => ({ tableLocation, data: tableLocation.schedules }))
 
   return (
     <View style={styles.container}>
@@ -53,8 +61,58 @@ const ShowIrnTablesContent: React.FunctionComponent<AppScreenProps> = () => {
   )
 }
 
+const TimesList: React.FunctionComponent<{ times: Time[] }> = ({ times }) => (
+  <View style={styles.timesListContainer}>
+    {sort(sortTimes, times).map(time => (
+      <TimeSlot key={time} time={time} />
+    ))}
+  </View>
+)
+
+const TimeSlot: React.FunctionComponent<{ time: Time }> = ({ time }) => (
+  <View style={styles.timeSlotContainer}>
+    <Text style={styles.timeSlot}>{formatTime(time)}</Text>
+  </View>
+)
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  districtContainer: {
+    alignItems: "center",
+    backgroundColor: "#93cbfa",
+    padding: 10,
+  },
+  districtText: {
+    fontSize: 16,
+  },
+  locationText: {
+    fontSize: 12,
+  },
+  scheduleContainer: {
+    padding: 10,
+  },
+  dateContainer: {
+    alignItems: "center",
+    backgroundColor: "#b4d9fa",
+    borderWidth: StyleSheet.hairlineWidth,
+  },
+  timesListContainer: {
+    flex: 1,
+    flexWrap: "wrap",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  timeSlotContainer: {
+    backgroundColor: "#d7ebfc",
+    borderWidth: StyleSheet.hairlineWidth,
+    padding: 5,
+    margin: 5,
+  },
+  timeSlot: {
+    fontSize: 10,
   },
 })
---
+++ src/utils/formaters.ts
@@ -0,0 +1,29 @@
+import moment from "moment-with-locales-es6"
+import { Time } from "../irnTables/models"
+
+const dayNames = ["Hoje", "Amanhã", "Depois de Amanhã"]
+
+export const formatLocale = (date: Date) => {
+  moment.locale("pt")
+  return moment(date).format("LL")
+}
+
+export const formatDate = (date: Date) => {
+  const diffDays = moment(date).diff(moment.now(), "days")
+  return diffDays >= 0 && diffDays <= 2 ? dayNames[diffDays] : formatLocale(date)
+}
+
+const t = (v: number) => `${v < 10 ? 0 : ""}${v}`
+
+export const formatTime = (time: Time) => {
+  const date = new Date(`2000-01-01T${time}`)
+  return `${t(date.getHours())}: ${t(date.getMinutes())}`
+}
+
+export const properCase = (s: string) =>
+  s
+    .trim()
+    .toLowerCase()
+    .split(" ")
+    .map(w => `${w[0].toUpperCase()}${w.substring(1)}`)
+    .join(" ")
