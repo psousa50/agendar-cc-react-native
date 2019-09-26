14ea9b36087028af21cb947d1a8211567f5fd601
Pedro Sousa <psousa@equalexperts.com>
2019-09-26 09:59:09 +0100

    Fix dates diff

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -2,15 +2,17 @@ import moment from "moment"
 
 export const addDays = (date: Date, days: number) => new Date(date.getTime() + days * 24 * 60 * 60 * 1000)
 
+export const diffDays = (d1: Date, d2: Date) =>
+  moment(d2)
+    .startOf("day")
+    .diff(moment(d1).startOf("day"), "days")
+
 export const datesEqual = (d1: Date, d2: Date) =>
   moment(d1)
-    .utc()
-    .isSame(moment(d2).utc())
+    .startOf("day")
+    .isSame(moment(d2).startOf("day"))
 
 export const createDateRange = (startDate: Date, endDate: Date) => {
-  const nrDays =
-    moment(endDate)
-      .utc()
-      .diff(moment(startDate).utc(), "days") + 1
+  const nrDays = diffDays(startDate, endDate) + 1
   return nrDays > 0 ? new Array(nrDays).fill(startDate).map((_, i) => addDays(startDate, i)) : []
 }
--- src/utils/formaters.ts
+++ src/utils/formaters.ts
@@ -2,10 +2,11 @@ import moment from "moment"
 import momentLoc from "moment-with-locales-es6"
 
 import { County, District, TimeSlot } from "../irnTables/models"
+import { diffDays as calcDiffDays } from "./dates"
 
 const dayNames = ["Hoje", "Amanhã", "Depois de Amanhã"]
 
-export const formatLocale = (date: Date) => {
+export const formatDateLocale = (date: Date) => {
   momentLoc.locale("pt")
   return momentLoc(date).format("LL")
 }
@@ -13,8 +14,8 @@ export const formatLocale = (date: Date) => {
 export const formatDateYYYYMMDD = (date: Date) => moment(date).format("YYYY-MM-DD")
 
 export const formatDate = (date: Date) => {
-  const diffDays = moment(date).diff(moment.now(), "days")
-  return diffDays >= 0 && diffDays <= 2 ? dayNames[diffDays] : formatLocale(date)
+  const diffDays = calcDiffDays(new Date(Date.now()), date)
+  return diffDays >= 0 && diffDays <= 2 ? dayNames[diffDays] : formatDateLocale(date)
 }
 
 const twoDigits = (v: number) => `${v < 10 ? 0 : ""}${v}`
