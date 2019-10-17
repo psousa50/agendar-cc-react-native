d9d5ad3b28c9b88ffbee217a681ca5adbc650955
Pedro Sousa <psousa@equalexperts.com>
2019-10-17 15:30:08 +0100

    Fix App test

--- test/App.nospec.tsx
+++ test/App.nospec.tsx
@@ -3,8 +3,9 @@ import "react-native"
 
 // Note: test renderer must be required after react-native.
 import renderer from "react-test-renderer"
-import { App } from "../src/App"
+import { AppContainer } from "../src/App"
 
 it.skip("renders correctly", () => {
-  renderer.create(<App />)
+  const props = {} as any
+  renderer.create(<AppContainer {...props} />)
 })
