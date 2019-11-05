20404560b3d5ec808d3e217f25be78920e90497f
Pedro Sousa <psousa@equalexperts.com>
2019-11-05 14:02:34 +0000

    Fix issue with distanceRadiusKm

--- src/utils/location.ts
+++ src/utils/location.ts
@@ -120,6 +120,7 @@ export const normalizeLocation = (referenceDataProxy: ReferenceDataProxy, irnPla
     districtId: filteredDistricts.length === 1 ? filteredDistricts[0].districtId : location.districtId,
     countyId: filteredCounties.length === 1 ? filteredCounties[0].countyId : location.countyId,
     placeName: filteredIrnPlaces.length === 1 ? filteredIrnPlaces[0].name : location.placeName,
+    distanceRadiusKm: location.distanceRadiusKm,
     gpsLocation: location.gpsLocation,
   }
 }
