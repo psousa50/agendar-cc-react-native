3c7f70c410c64be4ecad762c7ed169fb6ddf690a
Pedro Sousa <psousa@equalexperts.com>
2019-11-25 22:37:27 +0000

    Remove disable of YellowBox warning (not needed anymore)

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,4 @@
 import { AppRegistry } from "react-native"
-import { YellowBox } from "react-native"
 import { App } from "./App"
-
-YellowBox.ignoreWarnings(["Warning: componentWillReceiveProps is deprecated"])
 
 AppRegistry.registerComponent("agendarCC", App)
